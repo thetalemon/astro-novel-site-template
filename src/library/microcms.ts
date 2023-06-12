945675434d27d35e943c63440af4cae6ae5c9f67
thetalemon <sasami0314@gmail.com>
2023-06-12 22:05:33 +0900

    init

---
+++ src/library/microcms.ts
@@ -0,0 +1,89 @@
+import { createClient, MicroCMSQueries } from 'microcms-js-sdk'
+const client = createClient({
+  serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
+  apiKey: import.meta.env.MICROCMS_API_KEY,
+})
+
+export type Novel = {
+  id: string
+  publishedAt: string
+  revisedAt: string
+  title: string
+  content: string
+}
+
+export type NovelResponse = {
+  totalCount: number
+  offset: number
+  limit: number
+  contents: Novel[]
+}
+
+export const getNovels = async (queries?: MicroCMSQueries) => {
+  return await client.get<NovelResponse>({ endpoint: 'novels', queries })
+}
+
+export const getNovelDetail = async (
+  contentId: string,
+  queries?: MicroCMSQueries
+) => {
+  return await client.getListDetail<Novel>({
+    endpoint: 'novels',
+    contentId,
+    queries,
+  })
+}
+
+export type Illust = {
+  id: string
+  publishedAt: string
+  revisedAt: string
+  title: string
+  caption: string
+  illust: {
+    url: string
+    height: number
+    width: number
+  }
+}
+
+export type IllustResponse = {
+  totalCount: number
+  offset: number
+  limit: number
+  contents: Novel[]
+}
+
+export const getIllustraitons = async (queries?: MicroCMSQueries) => {
+  return await client.get<IllustResponse>({
+    endpoint: 'illustrations',
+    queries,
+  })
+}
+
+export const getIllustraitonDetail = async (
+  contentId: string,
+  queries?: MicroCMSQueries
+) => {
+  return await client.getListDetail<Novel>({
+    endpoint: 'illustrations',
+    contentId,
+    queries,
+  })
+}
+
+export type Category = {
+  id: string
+  name: string
+}
+
+export type CategoryResponse = {
+  totalCount: number
+  offset: number
+  limit: number
+  contents: Category[]
+}
+
+export const getCategories = async (queries?: MicroCMSQueries) => {
+  return await client.get<CategoryResponse>({ endpoint: 'categories', queries })
+}
